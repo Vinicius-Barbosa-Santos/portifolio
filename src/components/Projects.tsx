2376dae9e0c9d2acf14648a046aab441c4e6cedf
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-08-07 10:53:38 -0300

    Update Profile Description

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -34,7 +34,6 @@ export default function Projects() {
     },
     {
       title: "Projeto Xadrez",
-      description: "Descrição do projeto 5",
       image:
         "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8bW9iaWxlJTIwYXBwfGVufDB8fDB8fHww&auto=format&fit=crop&w=500&q=60",
       link: "https://chess-dev.netlify.app/",
